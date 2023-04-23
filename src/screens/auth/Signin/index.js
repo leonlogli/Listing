c1dd40fa963eb82c03f5f564fa3f13530c56e170
Léon Logli <leonlogli@gmail.com>
2023-04-24 01:12:29 +0200

    Setup google login

--- src/screens/auth/Signin/index.js
+++ src/screens/auth/Signin/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ScrollView, Text } from 'react-native';
 import AuthHeader from '../../../components/AuthHeader';
 import Button from '../../../components/Button';
